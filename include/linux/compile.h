407659a53c30c97fc59ebc4c762cb630825b6045
Dan Talley <talley_daniel@yahoo.com>
2012-05-31 10:25:29 -0400

    [Block]Added BFQ and VR  I/O Schedulers

---
+++ include/linux/compile.h
@@ -0,0 +1,8 @@
+/* This file is auto generated, version 28 */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#28 Thu May 31 10:06:32 EDT 2012"
+#define LINUX_COMPILE_TIME "10:06:32"
+#define LINUX_COMPILE_BY "dan"
+#define LINUX_COMPILE_HOST "dan-VirtualBox"
+#define LINUX_COMPILE_DOMAIN
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-68) "
